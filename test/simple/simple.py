a9b16efc26ba41095d33f4857507c5c04ca93172
shuibing <191098191@smail.nju.edu.cn>
2023-10-16 17:37:56 +0800

    [test/simple] support vtysh, need handle different DB

--- test/simple/simple.py
+++ test/simple/simple.py
@@ -7,6 +7,7 @@ import os
 from mininet import log
 from os import path
 import signal
+import shutil
 from mininet.cli import CLI
 
 BIN_DIR="/usr/lib/frr"
@@ -25,17 +26,24 @@ def kill_pid(pid:int):
 
 
 class FrrNode(Node):
-    def load_frr(self, daemons, work_dir):
+    def load_frr(self, daemons, conf_dir):
+        mkdir_cmds = ["mkdir", "-p", f"/home/frr/vtysh"]
+        self.cmd_frr_error(mkdir_cmds)
+        self.vtysh_dir = f"/home/frr/vtysh/{self.name}/"
+        mkdir_cmds = ["mkdir", "-p", self.vtysh_dir]
+        #FIXME: ospf.db zebra.db zserv.api is not handle correctly
+
+        self.cmd_frr_error(mkdir_cmds)
         self.daemon_dict = {}
         for daemon in daemons:
-            self._load_daemon(daemon, work_dir)
+            self._load_daemon(daemon, conf_dir)
 
     def _load_daemon(self, daemon_name, work_dir: str):
         pid_path = path.join("/tmp", f"{self.name}-{daemon_name}.pid")
         log_path = path.join("/tmp", f"{self.name}-{daemon_name}.log")
         conf_path = path.join(work_dir, f"{self.name}.conf")
         self.daemon_dict[daemon_name] = {"pid_path":pid_path, "log_path":log_path, "conf_path":conf_path}
-        self.cmd_error([f"{BIN_DIR}/{daemon_name}", "-f", conf_path, "-d", "-i", pid_path, ">", log_path, "2>&1"])
+        self.cmd_error([f"{BIN_DIR}/{daemon_name}", "-f", conf_path, "-d", "-i", pid_path, "--vty_socket", self.vtysh_dir, ">", log_path, "2>&1"])
         with open(pid_path, "r") as file:
             daemon_pid = int(file.read())
             self.daemon_dict[daemon_name]["daemon_pid"] = daemon_pid
@@ -44,6 +52,9 @@ class FrrNode(Node):
         for v in self.daemon_dict.values():
             kill_pid(v["daemon_pid"])
             os.remove(v["pid_path"])
+        if path.exists(self.vtysh_dir):
+            shutil.rmtree(self.vtysh_dir)
+        log.info("cleaned\n")
         super().terminate()
 
     def cmd_frr_error(self, cmds):
@@ -61,14 +72,12 @@ class FrrNode(Node):
             log.error(f"error: {e}")
             exit()
 
-    def daemon_multicmd(self, daemon_name, cmds):
-        # cmds_str = " ".join([f'-c "{cmd}"' for cmd in cmds])
-        cmds_list = []
-        for cmd in cmds:
-            cmds_list.append("-c")
-            cmds_list.append(cmd)
-        res = self.pexec(["vtysh", "-d", daemon_name])
-        _ = res
+    def daemon_multicmd(self,  cmds, daemon_name = None):
+        cmds_list = ["vtysh", "--vty_socket", self.vtysh_dir]
+        if daemon_name is not None:
+            cmds_list = cmds_list + ["-d", daemon_name]
+        cmds_list = cmds_list + [f'-c "{cmd}"' for cmd in cmds]
+        return self.cmd_error(cmds_list)
 
 def simpleTest():
     topo = Topo()
@@ -79,13 +88,17 @@ def simpleTest():
     topo.addLink(r2, r3)
     #build network
     net = Mininet(topo)
-    net.nameToNode["r1"].load_frr(daemons=["zebra", "ospfd"], work_dir=WORK_DIR)
-    net.nameToNode["r2"].load_frr(daemons=["zebra", "ospfd"], work_dir=WORK_DIR)
-    net.nameToNode["r3"].load_frr(daemons=["zebra", "ospfd"], work_dir=WORK_DIR)
+    net.nameToNode["r1"].load_frr(daemons=["zebra", "ospfd"], conf_dir=WORK_DIR)
+    net.nameToNode["r2"].load_frr(daemons=["zebra", "ospfd"], conf_dir=WORK_DIR)
+    net.nameToNode["r3"].load_frr(daemons=["zebra", "ospfd"], conf_dir=WORK_DIR)
     try:
         net.start()
-        CLI(net)
-    except:
+        sleep(30)
+        net.nameToNode["r1"].daemon_multicmd(["show ip ospf route"])
+        while(1):
+            pass
+    except BaseException as e:
+        log.error(f"{e}\n")
         net.stop()
 
 
