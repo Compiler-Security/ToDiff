79c1b9cf98d1500b2dd0d558e7873c93eed72c53
shuibing <191098191@smail.nju.edu.cn>
2023-10-29 20:24:52 +0800

    [fuzzer] start ops

--- src/fuzzer/operation/ops.cpp
+++ src/fuzzer/operation/ops.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "ops.h"
+enum OpTarget{
+    physic,
+    ospf
+};
+
+enum PHYOp{
+
+};
+
+enum OSPFOp{
+
+};
+
+class BaseOperation{
+
+};
+
+class PHYOperation:BaseOperation{
+
+};
+
+class PNodeAdd:PHYOperation{
+
+};
+
+class PNodeDel:PHYOperation{
+
+};
+
+class PNodeSet:PHYOperation{
+
+};
+
+class PIntfUp:PHYOperation{
+
+};
+
+class PIntfDown:PHYOperation{
+
+};
+
+class PIntfSet:PHYOperation{
+
+};
+
+class PLinkUp:PHYOperation{
+
+};
+
+class PLinkDown:PHYOperation{
+
+};
+
+class PLinkRemove:PHYOperation{
+    
+};
+
+
