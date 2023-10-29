a4d3f5da7f359286d7a81de433e8682b9eb7a17c
shuibing <191098191@smail.nju.edu.cn>
2023-10-29 16:33:37 +0800

    [fuzzer] start fuzzer

---
+++ src/fuzzer/operation/ops.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 水兵 on 2023/10/29.
+//
+
+#include "ops.h"
---
+++ src/fuzzer/operation/ops.h
@@ -0,0 +1,8 @@
+//
+// Created by 水兵 on 2023/10/29.
+//
+
+#ifndef FUZZER_OPS_H
+#define FUZZER_OPS_H
+
+#endif //FUZZER_OPS_H
