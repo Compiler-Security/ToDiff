ace51a5bfa3b2c2d4d3ade7731b8c4a2e8fe09ac
shuibing <191098191@smail.nju.edu.cn>
2023-11-04 18:03:56 +0800

    [gradle] add JUnit

--- src/generator/build.gradle.kts
+++ src/generator/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("junit:junit:4.13.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.testng:testng:7.7.0")
