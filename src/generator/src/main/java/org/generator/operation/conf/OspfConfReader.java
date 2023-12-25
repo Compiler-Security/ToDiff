93a630a154d2188119445ed5bf2b105471629f50
Bing Shui <652023330037@smail.nju.edu.cn>
2023-12-25 21:43:39 +0800

    [src/generator/conf/OspfConfReader] remove unnecessary op checks

--- src/generator/src/main/java/org/generator/operation/conf/OspfConfReader.java
+++ src/generator/src/main/java/org/generator/operation/conf/OspfConfReader.java
@@ -3,7 +3,6 @@ package org.generator.operation.conf;
 import org.generator.operation.op.OpGen;
 import org.generator.operation.op.OpType;
 import org.generator.operation.op.Operation;
-import org.generator.util.collections.Pair;
 import org.generator.util.net.IPV4;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,8 +10,6 @@ import java.io.*;
 import java.util.*;
 
 public class OspfConfReader implements ConfR {
-
-
     public  Operation changeIDNumToID(Operation op, OpType target_type){
         if (0 <= op.getIDNUM() && op.getIDNUM() <= 4294967295L){
             var new_op = op.cloneOfType(target_type);
@@ -24,113 +21,33 @@ public class OspfConfReader implements ConfR {
         }
     }
 
-    private Operation invalid(){
-        return new Operation(OpType.INVALID);
-    }
-
-    private boolean inRange(long num, long l, long r){
-        return l <= num && num <= r;
-    }
-
-    private Operation checkNum(Operation op, long l, long r){
-        return op.checkOrInvalid(x -> inRange(x.getNUM(), l, r));
-    }
-
-    private Operation checkLongNum(Operation op, long l, long r){
-        return op.checkOrInvalid(x -> inRange(x.getIDNUM(), l, r));
-    }
-    private Operation checkOperation(Operation op){
+    private Operation tranverseOperation(Operation op){
         if (OpType.inPhy(op.Type())) return op;
         switch (op.Type()){
             case INVALID, ROSPF,RID, NETAREAID, PASSIVEINTFDEFUALT -> {return op;}
-            case RABRTYPE -> {
-                String[] type = {"standard", "Cisco", "IBM", "shortcut"};
-                var name = op.getNAME();
-                if (!Arrays.stream(type).anyMatch(x -> x.equals(name))){
-                    return invalid();
-                }
-                return op;
-            }
             case NETAREAIDNUM -> {
-                return checkOperation(changeIDNumToID(op, OpType.NETAREAID));
-            }
-            case TIMERSTHROTTLESPF -> {
-                return op.checkOrInvalid(x -> inRange(x.getNUM(), 0, 600000), x -> inRange(x.getNUM2(), 0, 600000), x -> inRange(x.getNUM3(), 0, 600000));
-            }
-            case CLEARIPOSPFPROCESS,CLEARIPOSPFNEIGHBOR -> {
-                return op;
-            }
-            case MAXIMUMPATHS -> {
-                return checkNum(op, 1, 64);
-            }
-            case WRITEMULTIPLIER -> {
-                return checkNum(op, 1, 100);
-            }
-            case SOCKETBUFFERALL,SOCKETBUFFERRECV,SOCKETBUFFERSEND -> {
-                return checkLongNum(op, 1, 4000000000L);
-            }
-            case NOSOCKETPERINTERFACE -> {return op;}
-            //case AreaRangeAd,
-            case AreaRange, AreaRangeNoAd,AreaRangeSub -> {return op;}
-            //case AreaRangeAdCost,
-                    case AreaRangeCost -> {
-                return checkNum(op, 0, 16777215);
+                return changeIDNumToID(op, OpType.NETAREAID);
             }
             case AreaRangeINT -> {
-                return checkOperation(changeIDNumToID(op, OpType.AreaRange));
+                return changeIDNumToID(op, OpType.AreaRange);
             }
-//            case AreaRangeAdINT -> {
-//                return checkOperation(changeIDNumToID(op, OpType.AreaRangeAd));
-//            }
-//            case AreaRangeAdCostINT -> {
-//                return checkOperation(changeIDNumToID(op, OpType.AreaRangeAdCost));
-//            }
             case AreaRangeNoAdINT -> {
-                return checkOperation(changeIDNumToID(op, OpType.AreaRangeNoAd));
+                return changeIDNumToID(op, OpType.AreaRangeNoAd);
             }
             case AreaRangeSubINT -> {
-                return checkOperation(changeIDNumToID(op, OpType.AreaRangeSub));
+                return changeIDNumToID(op, OpType.AreaRangeSub);
             }
             case AreaRangeCostINT -> {
-                return checkOperation(changeIDNumToID(op, OpType.AreaRangeCost));
+                return changeIDNumToID(op, OpType.AreaRangeCost);
             }
             case AreaVLink -> {
                 //FIXME areavlink don't implement
                 assert false: "area vlink don't implement";
             }
-            case AreaShortcut -> {
-                String[] type = {"enable", "disable", "default"};
-                var name = op.getNAME();
-                if (!Arrays.stream(type).anyMatch(x -> x.equals(name))){
-                    return invalid();
-                }
-                return op;
-            }
-            case AreaStub, AreaStubTotal, AreaNSSA -> {return op;}
-            case IntfName,IPAddr,IpOspfArea -> {return op;}
             case IpOspfAreaINT -> {
-                return checkOperation(changeIDNumToID(op, OpType.IpOspfArea));
+                return changeIDNumToID(op, OpType.IpOspfArea);
             }
-            case IpOspfCost,IpOspfDeadInter,IpOspfHelloInter,IpOspfRetransInter,IpOspfTransDealy -> {
-                return checkNum(op, 1, 65535);
-            }
-            case IpOspfDeadInterMulti -> {
-                return checkNum(op, 2, 20);
-            }
-            case IpOspfGRHelloDelay -> {
-                return checkNum(op, 1, 1800);
-            }
-            case IpOspfNet -> {
-                if (op.getNAME().equals("broadcast") || op.getNAME().equals("non-broadcast")){
-                    return op;
-                }else{
-                    return invalid();
-                }
-            }
-            case IpOspfPriority -> {
-                return checkNum(op, 0, 255);
-            }
-            case IpOspfPassive -> {
+            default -> {
                 return op;
             }
         };
@@ -142,7 +59,7 @@ public class OspfConfReader implements ConfR {
         for (var v: OpType.getMatchOps()){
             var op = OpGen.GenOperation(v);
             if (op.decode(op_st)){
-                return checkOperation(op);
+                return tranverseOperation(op);
             }
         }
         return null;
