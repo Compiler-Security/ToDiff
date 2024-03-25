9dca01f0d48afe6487e183097a809f321e36b43f
Bing Shui <652023330037@smail.nju.edu.cn>
2024-03-25 17:04:10 +0800

    [src/generator/reducer] fix reducer bug

--- src/generator/src/main/java/org/generator/lib/reducer/pass/reducePass.java
+++ src/generator/src/main/java/org/generator/lib/reducer/pass/reducePass.java
@@ -80,8 +80,8 @@ public class reducePass {
     }
 
     private static boolean conflict(OpAnalysis preOpa, OpAnalysis opa){
-        //ctx_op should equal except IpOspfArea
-        if (opa.getOp().Type() == OpType.IpOspfArea || opa.getOp().Type() == OpType.NETAREAID) return matchByRedexDef(opa, preOpa, ConflictRedexDef.getRdcDef(opa.getOp().Type()));
+        //ctx_op should equal except IpOspfArea vs. networkAreaId
+        if ((opa.getOp().Type() == OpType.IpOspfArea && preOpa.getOp().Type() == OpType.NETAREAID) || (preOpa.getOp().Type() == OpType.IpOspfArea && opa.getOp().Type() == OpType.NETAREAID)) return matchByRedexDef(opa, preOpa, ConflictRedexDef.getRdcDef(opa.getOp().Type()));
         if (!CtxOpDef.isCtxOp(opa.op.Type()) && !preOpa.getCtxOp().getOp().equals(opa.getCtxOp().getOp())){return false;}
         return matchByRedexDef(opa, preOpa, ConflictRedexDef.getRdcDef(opa.getOp().Type()));
     }
