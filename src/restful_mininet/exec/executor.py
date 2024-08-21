0dd40576221a2d3895477f7bafb289eeac272ff1
Your Name <you@example.com>
2024-08-21 13:19:29 +0000

    [restful_mininet] change testhalt test name

--- src/restful_mininet/exec/executor.py
+++ src/restful_mininet/exec/executor.py
@@ -129,15 +129,26 @@ class executor:
             if sleep_time == -1:
                 #FIXME this should check shrink
                 #time.sleep(20)
-                time.sleep(20)
+                #time.sleep(20)
                 #while(not self._check_converge(net)): time.sleep(5)
           
                 #CLI(net.net)
-                #time.sleep(80)
+                time.sleep(1)
                 #time.sleep(80)
                 # for r_name in self.routers:
                 #     print(net.net.nameToNode[r_name].daemon_cmds(["show ip ospf neighbor"]))
                 #     print(net.net.nameToNode[r_name].daemon_cmds(["show running-config"]))
+
+
+                #handle convergence
+                if r == 0:
+                    #when r == 0, this means the topo is not change and in the final state, so we just wait convergence
+                    #the check convergence algorithm is:
+                        #1.First we check all the interfaces' neighbor list, when find all the neighbor's state is full
+
+                    pass
+                else:
+                    pass
             else:
                 #CLI(net.net)
                 time.sleep(sleep_time)
@@ -151,5 +162,5 @@ class executor:
         return res
     
 if __name__ == "__main__":
-    t = executor("/home/frr/topo-fuzz/test/excutor_test/frr_conf/all8.conf", "/home/frr/topo-fuzz/test/excutor_test/tmp")
+    t = executor("/home/frr/topo-fuzz/test/excutor_test/frr_conf/testhalt.conf", "/home/frr/topo-fuzz/test/excutor_test/tmp")
     t.test()
