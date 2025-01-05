0981c9760b451e7f6eed07a9f302d16b405dd7fc
Z-Yivon <202100460108@mail.sdu.edn.cn>
2025-01-05 14:57:57 +0800

    fix the bug: L1-2 route should in the area that has L1 route

--- src/generator/src/main/java/org/generator/lib/topo/pass/base/ranBaseGen_ISIS.java
+++ src/generator/src/main/java/org/generator/lib/topo/pass/base/ranBaseGen_ISIS.java
@@ -126,20 +126,23 @@ public class ranBaseGen_ISIS implements genBase_ISIS {
         routers = new ArrayList<>();
         networkId = 0;
         Set<Integer> actualArea = new HashSet<>();
+        var backboneRouters = new ArrayList<Router_ISIS>();
         // first stage: generate routers
         for (int i = 0; i < totalRouter; i++) {
             int level = ranHelper.randomInt(0, 1); // 0: L1, 1: L2, 2: L1-2
             int area;
+            int tmp_area = ranHelper.randomInt(1, areaCount);
             if (level == 0) { // L1 router is in non-backbone area
-                area = ranHelper.randomInt(1, areaCount);
-            } else { // L2 router and L1-2 router are in backbone area
+                area = tmp_area;
+            } else { // L2 router is in backbone area
                 area = 0;
             }
             var intfNum = ranHelper.randomInt(1, mxDegree);
             var isL12 = (ranHelper.randomInt(1, 10) <= abrRatio) && intfNum > 1;
-            if (isL12) {
+            var L12_num = getRoutersOfArea(tmp_area).stream().filter(r -> r.level == 2).count();
+            if (isL12 && L12_num == 0) {
                 level = 2;
-                area = 0;
+                area = tmp_area;
             }
             var r = new Router_ISIS(i, level, area);
             routers.add(r);
@@ -161,14 +164,19 @@ public class ranBaseGen_ISIS implements genBase_ISIS {
             r.area = renumber.get(r.area);
         }
         areaCount = ns - 1;
-        //make sure all routers in the backbone area are L2 or L1-2 routers
-        var backboneRouters = getRoutersOfArea(0);
-        for (var r : backboneRouters) {
-            if (r.level == 0) {
-                r.level = 2;
+        //make sure all routers in the area 0 are L2 routers
+        var l2Routers = getRoutersOfArea(0);
+        for (var r : l2Routers) {
+            if (r.level == 0 || r.level == 2) {
+                r.level = 1;
             }
         }
 
+        //add all L2 routers in the backbone area
+        for(var r : l2Routers){
+            backboneRouters.add(r);
+        }
+        
 
         // second stage: connect routers in the same non-backbone area
         for (int i = 1; i <= areaCount; i++) {
@@ -178,63 +186,76 @@ public class ranBaseGen_ISIS implements genBase_ISIS {
             networkId = c.networkId;
         }
 
-        // third stage: connect L1 and L1-2 routers 
-        backboneRouters = getRoutersOfArea(0);
-        var l12Routers = backboneRouters.stream()
-        .filter(r -> r.level == 2)
-        .collect(Collectors.toList());
+        // third stage: connect L1-2 with L1 and add L1-2 in the backbone area
+        l2Routers = backboneRouters.stream()
+            .filter(r -> r.level == 1)
+            .collect(Collectors.toList());
         for(int i = 1; i <= areaCount; i++) {
             var areaRouters = getRoutersOfArea(i);
             if(areaRouters.isEmpty()) continue;
-            
-            // choose a router in the non-backbone area
-            var selectedRouter = ranHelper.randomElemsOfList(areaRouters, 1).get(0);
-            
-            if(l12Routers.isEmpty()) {
-                // if there is no L1-2 router, let the selected router become a L1-2 router
+            var l12Routers = areaRouters.stream()
+                .filter(r -> r.level == 2)
+                .collect(Collectors.toList());
+            if(l12Routers.isEmpty()){
+                // if there is no L1-2 router, choose a L1 router to become a L1-2 router
+                var selectedRouter = ranHelper.randomElemsOfList(areaRouters, 1).get(0);
                 selectedRouter.level = 2;
-                selectedRouter.area = 0;
                 l12Routers.add(selectedRouter);
-            } else {
-                // choose a L1-2 router
-                var l12Router = ranHelper.randomElemsOfList(l12Routers, 1).get(0);
+            }
+            if (l12Routers.size()>1) {
+                throw new RuntimeException("L1-2 routers are more than 1");
                 
-                // add interfaces to connect the selected router and the L1-2 router
-                var intf1 = new Intf_ISIS();
-                var intf2 = new Intf_ISIS();
-                intf1.cost = ranHelper.randomInt(1, 65535);
-                intf2.cost = ranHelper.randomInt(1, 65535);
-                selectedRouter.intfs.add(intf1);
-                l12Router.intfs.add(intf2);
-                
-                // set the network id for the interfaces
-                intf1.networkId = networkId;
-                intf2.networkId = networkId;
-                networkId++;
+            }
+            else{
+                // if all the intfs of L1-2 routers are not connected, choose a L1 router to connect with L1-2 routers
+                var connectedL12Intfs = l12Routers.stream().flatMap(r -> r.getConnectedIntfs().stream()).collect(Collectors.toList());
+                if(connectedL12Intfs.isEmpty()){
+                    while(true){
+                        var selectedRouter = ranHelper.randomElemsOfList(areaRouters, 1).get(0);
+                        if(l12Routers.contains(selectedRouter)){
+                            continue;
+                        }
+                        // add intfs
+                        var intf1 = new Intf_ISIS();
+                        var intf2 = new Intf_ISIS();
+                        intf1.cost = ranHelper.randomInt(1, 65535);
+                        intf2.cost = ranHelper.randomInt(1, 65535);
+                        selectedRouter.intfs.add(intf1);
+                        l12Routers.get(0).intfs.add(intf2);
+
+                        // set networkId
+                        intf1.networkId = networkId;
+                        intf2.networkId = networkId;
+                        networkId++;
+                    }
+                }
+            }
+            for (var r : l12Routers) {
+                backboneRouters.add(r);
             }
         }  
 
 
 
         // fourth stage: connect L1-2 routers
-        backboneRouters = getRoutersOfArea(0);
-        l12Routers = backboneRouters.stream()
-            .filter(r -> r.level == 2)
-            .collect(Collectors.toList());
-        if(l12Routers.isEmpty() == false) {
-            var c = new ranConnect_ISIS();
-            c.generate(l12Routers, 0, networkId);  
-            networkId = c.networkId;
-        }
+        // var alll12Routers = backboneRouters.stream()
+        //     .filter(r -> r.level == 2)
+        //     .collect(Collectors.toList());
+
+        // if(alll12Routers.isEmpty() == false) {
+        //     var c = new ranConnect_ISIS();
+        //     c.generate(alll12Routers, 0, networkId);  
+        //     networkId = c.networkId;
+        // }
 
 
         // fifth stage: connect routers in the backbone area 
-        var l2Routers = backboneRouters.stream()
-        .filter(r -> r.level == 1 || r.level == 2)
-        .collect(Collectors.toList());
+        var allbackbonerouters = backboneRouters.stream()
+            .filter(r -> r.level == 1||r.level == 2)
+            .collect(Collectors.toList());
 
         var c = new ranConnect_ISIS();
-        c.generate(l2Routers, 0, networkId); 
+        c.generate(allbackbonerouters, 0, networkId); 
         networkId = c.networkId;
         
         return routers;
