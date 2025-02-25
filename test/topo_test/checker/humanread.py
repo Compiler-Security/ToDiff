7aa989cdffa9aa5f4754aaa2c5d8d208dec1299b
Shbinging <bingshui@smail.nju.edu.cn>
2025-02-25 09:06:25 +0000

    [checker] update

--- test/topo_test/checker/humanread.py
+++ test/topo_test/checker/humanread.py
@@ -129,7 +129,7 @@ import util
 if __name__ == "__main__":
     #rd from 0
     #human_read_ospf(0, 0, "/home/frr/topo-fuzz/test/topo_test/data/result/test1726036744_r1/test1726036744_r1_res.json")
-    test_name = util.get_test_name_5("88327")
+    test_name = util.get_test_name_5("72514")
     h = humandReader(util.get_result_file_path(test_name))
     h.readAll()
 
